c97b9d447958bd0769f4b78cee09312167ddb9b6
Adarsh Jaiswal <adarshj@emiprotechnologies.com>
2024-02-02 10:24:33 +0530

    [MOD] Added required name modifications in the code

--- crm_lead_demo_ept/__manifest__.py
+++ crm_lead_demo_ept/__manifest__.py
@@ -5,7 +5,7 @@
     'sequence':-5,
     'summary':'CRM Practice Module',
     'description':'This module is for practice purpose only!!',
-    'depends':['sales_team'],
+    'depends':[],
     'data':[
         'security/crm_lead_demo_ept_rule.xml',
         'security/crm_lead_demo_ept_groups.xml',
